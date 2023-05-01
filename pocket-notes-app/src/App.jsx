c44a110f2f830579d3869e692b349ca47d35c2eb
Purujit Pradhan <purujit26@gmail.com>
2023-05-01 18:08:47 +0530

    first empty image

--- pocket-notes-app/src/App.jsx
+++ pocket-notes-app/src/App.jsx
@@ -1,41 +1,40 @@
 import MediaQuery from "react-responsive";
 import "./App.css";
+import Standby from "./assets/standby.png";
+import Lock from "./assets/lock.png"
 
 function App() {
   return (
-    
     <>
       <MediaQuery maxWidth={767}>
-        <div className="mobile">
+        {/*<div className="mobile">
           <div className="header">Pocket Notes</div>
           <div className="create-button">
             <button>+ Create Notes</button>
           </div>
           <div className="notes-heading">
             <div className="title-half">
-              {/* this area will contain the name of the app,
+              this area will contain the name of the app,
               it will also contian, the create button,
-              and the created notes with their title */}
+              and the created notes with their title 
               <div className="app-name">Pocket Notes</div>
               <div className="create-btn-div">
                 <button className="createBtn"></button>
               </div>
-              <div className="notes-title-area">
-                <>a prop will go here for the title</>
-              </div>
+              
             </div>
             <div className="notes-half">
               <div className="notes header">
                 <div className="empt-area">
                   <div className="no-select-note-image">
-                    <img src="" alt="" />
+                    
                   </div>
                   <div>encryption text</div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </MediaQuery>
       <MediaQuery minWidth={768}>
         <div className="screen">
@@ -45,7 +44,7 @@ function App() {
               and the created notes with their title */}
             <div className="app-name">Pocket Notes</div>
             <div className="create-btn-div">
-              <button className="createBtn">+ Create Notes Group</button>
+              <button className="createBtn">+ Create Notes group</button>
             </div>
             <div className="notes-title-area">
               <>a prop will go here for the title</>
@@ -53,14 +52,17 @@ function App() {
           </div>
           <div className="notes-half">
             <div>
-              <img src="" alt="" />
+              <div>
+                <img className="standby-image" src={Standby} alt="image" />
+              </div>
+              <div className="app-name2">Pocket Notes</div>
+              <div className="text-under-name">
+                Send and receive messages without keeping your phone online. Use
+                Pocket Notes on up to 4 linked devices and 1 mobile phone
+              </div>
+              
             </div>
-            <div>Pocket Notes</div>
-            <div>
-              Send and receive messages without keeping your phone online. Use
-              Pocket Notes on up to 4 linked devices and 1 mobile phone
-            </div>
-            <div>end-to-end encrypted</div>
+            <div className="encryption-text"> <img className="tiny-lock" src={Lock} alt="" /> end-to-end encrypted</div>
           </div>
         </div>
       </MediaQuery>
